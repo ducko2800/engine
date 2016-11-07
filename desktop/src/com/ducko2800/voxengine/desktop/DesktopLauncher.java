45c9ff6d00cc9774aba89b112c29e65d27aa56b3
tiget <jjobrien12@gmail.com>
2016-11-07 19:54:07 +1000

    initial commit

---
+++ desktop/src/com/ducko2800/voxengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ducko2800.voxengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ducko2800.voxengine.voxengine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new voxengine(), config);
+	}
+}
