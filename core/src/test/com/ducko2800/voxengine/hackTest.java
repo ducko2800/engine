55364f188ac73a9958ae33fe7bb288e0dcd98634
mitchmcdee <mitchmcdee@gmail.com>
2016-11-09 20:49:41 +1000

    Refactored folder structure and added junit. Added simple test so bamboo chills out

---
+++ core/src/test/com/ducko2800/voxengine/hackTest.java
@@ -0,0 +1,12 @@
+package com.ducko2800.voxengine;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class hackTest {
+
+	@Test
+	public void isTrueReallyTrue() {
+		assertTrue(true);
+	}
+}
